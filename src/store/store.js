148c6f9c8e1ea84715664a52b93fab8401c62276
Igor <igor,zhuiko@mail.ru>
2022-11-13 23:02:32 +0300

    fix comment

--- src/store/store.js
+++ src/store/store.js
@@ -1,17 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { applyMiddleware, createStore, compose } from "redux";
 import { chatReducer } from "./slices/sliceFormChat";
 import{ profileReducer } from "./reducer"
 
-// это не нужно больше, так как встроенно в тулкит
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// export const store = createStore(profileReducer, composeEnhancers(applyMiddleware()))
-
-//код преподавателя
-
-// стор создаем через функцию configureStore из тулкита внутри reducer передаем все редьюсеры нашей программы
-// редьюсеры получаем из созданных слайсов -> смотрите комменты в слайс
 export const store = configureStore({
     reducer:{
         chats:chatReducer,
